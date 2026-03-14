ead0617211eec76ea4d369b5804f4a02385dedf3
Chieh <secret12501250@gmail.com>
2026-03-14 19:15:11 +0800

    refactor: split SuccessfulCase folder and extract data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import MainLayout from '../components/common/MainLayout';
 import HeaderMarquee from '../components/common/HeaderMarquee';
 import HomeHero from '../components/home/HomeHero';
 import AboutRocket from '../components/home/AboutRocket';
-import SuccessfulCase from '../components/home/SuccessfulCase';
+import SuccessfulCase from '../components/home/SuccessfulCase/SuccessfulCase';
 import AboutTraining from '../components/home/AboutTraining';
 import FAQ from '../components/home/FAQ';
 import Organizers from '../components/home/Organizers';
